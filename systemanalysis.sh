c5b3f7042ac9a955c35d88192e530586351a6cca
appbank4 <appbank22@gmail.com>
2022-11-20 10:51:52 -0500

    this is a new script

---
+++ systemanalysis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#This script will be use to perform system analysis
+echo -n "Please enter your name"
+read $yourname
+echo "WELCOME TO CYBER COUNTER"
+sleep 5
+sudo a apt update -y
+df -h
+lsblk
+free -m
+free -g
+top
+htop
+echo "Thank you for using my script"
